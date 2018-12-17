78f5e58b0ffaaf57600af80be3d41c45cef58e7c
user <user@vinodh>
2018-12-17 12:26:25 +0530

    Front End Integration with Back End projet

---
+++ ecommerceFE/src/main/java/controller/ProductController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import model.dao.IProductDAO;
+import model.daoimpl.ProductDAOImpl;
+import model.entity.Product;
+
+@Controller
+public class ProductController {
+
+	@RequestMapping(value="/addprodpage",method=RequestMethod.GET)
+	public String addProductPage()
+	{
+		return "addproducts";
+	}
+	
+	@RequestMapping(value="/addproducts",method=RequestMethod.POST)
+	public String addProducts(@ModelAttribute("product1")Product p)
+	{
+		IProductDAO pd = new ProductDAOImpl();
+		boolean b = pd.insertProduct(p);
+		if (b)
+		{
+			return "success";
+		}
+		else
+		{
+			return "addproducts";
+		}
+	}
+	
+}
---
+++ ecommerceFE/src/main/webapp/views/addproducts.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+<form action="${pageContext.request.contextPath}/addproducts" method="POST">
+
+Product Name: <input type="text" name="pName"/>
+
+Product Cost: <input type="text" name="pCost"/>
+
+<input type="submit" value="Submit"/>
+
+</form>
+
+</body>
+</html>
